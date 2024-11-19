fff5503ec2b12bb61bdbb468830e28a975488f1f
LleytonParsons <168110045+LleytonParsons@users.noreply.github.com>
2024-11-19 11:49:15 -0500

    Creates new failing test intToRomanTestStage2() in InputManagerTest.java and renames intToRomanTest to intToRomanTestStage1(). Higher stages will test larger integer inputs and Roman numerals.

--- src/test/java/edu/bsu/cs222/InputManagerTest.java
+++ src/test/java/edu/bsu/cs222/InputManagerTest.java
@@ -12,12 +12,24 @@ public class InputManagerTest {
     }
 
     @Test
-    public void intToRomanTest(){
+    //Tests the smallest basic inputs between 1 and 3.
+    public void intToRomanTestStage1(){
         int testNumber = 3;
         String expected = "III";
 
         InputManager converter = new InputManager();
         String testResult = converter.intToRoman(testNumber);
         assertEquals(expected, testResult);
+    }
+
+    @Test
+    //Tests small integer inputs that use the Roman numeral 'V' (4 to 8).
+    public void intToRomanTestStage2(){
+        int testNumber = 5;
+        String expected = "V";
+
+        InputManager converter = new InputManager();
+        String testResult = converter.intToRoman(testNumber);
+        assertEquals(expected,testResult);
     }
 }
