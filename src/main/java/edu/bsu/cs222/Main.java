118023f3ccf0debfae9cc75109be860733720410
LleytonParsons <168110045+LleytonParsons@users.noreply.github.com>
2024-11-20 13:03:38 -0500

    Adds onto Main class using the rest of the intToRoman functions in a structured flow/loop. May need revisions.

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -14,9 +14,35 @@ public class Main {
             System.out.println("Number inputted was less than 1 or greater than 3,000. Please try again.\n");
         }
         else {
-            while(userInput >0) {
-                romanNumeral = inputConverter.intToRomanM(userInput);
-                userInput -= 1000;
+            while(userInput > 0) {
+                if(userInput >= 1000) {
+                    romanNumeral += inputConverter.intToRomanM(userInput);
+                    userInput -= 1000;
+                }
+                else if(userInput >=500) {
+                    romanNumeral += inputConverter.intToRomanD(userInput);
+                    userInput -= 500;
+                }
+                else if(userInput >= 100) {
+                    romanNumeral += inputConverter.intToRomanC(userInput);
+                    userInput -= 100;
+                }
+                else if(userInput >= 50) {
+                    romanNumeral += inputConverter.intToRomanL(userInput);
+                    userInput -= 50;
+                }
+                else if(userInput >= 10){
+                    romanNumeral += inputConverter.intToRomanX(userInput);
+                    userInput -= 10;
+                }
+                else if (userInput >= 5){
+                    romanNumeral += inputConverter.intToRomanV(userInput);
+                    userInput -= 5;
+                }
+                else if (userInput >= 1){
+                    romanNumeral += inputConverter.intToRomanI(userInput);
+                    userInput -= 1;
+                }
             }
             System.out.println(romanNumeral);
         }
